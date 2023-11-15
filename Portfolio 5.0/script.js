590103d972533901a6394e82e8e0e9e2f7cc3b3b
jecapreperation <150924530+jecapreperation@users.noreply.github.com>
2023-11-15 18:50:47 +0530

    Add files via upload

---
+++ Portfolio 5.0/script.js
@@ -0,0 +1,5 @@
+function sendEmail() {
+    // You can implement email sending functionality here using a service like Email.js or similar.
+    // This is just a placeholder function.
+    alert("Email functionality is not implemented in this example.");
+}
